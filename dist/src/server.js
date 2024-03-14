03d2979110765452a5410fe046c33e7264efa2cc
Sandesh Paudel <125977185+Sandesh-cmd@users.noreply.github.com>
2024-03-14 12:07:28 +0000

    deleted some js files and fixed error

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,18 +1,14 @@
 import express from "express";
 import weatherRoute from "./routes/weatherRoutes.js";
-
 // We will create an express app
 const app = express();
-
 //the port that the express server will listen on
 const PORT = 3000;
-
-app.use(express.json())
-
+app.use(express.json());
 //our new route
 app.use("/api/weather", weatherRoute);
-
 //start the express server
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`);
 });
+//# sourceMappingURL=server.js.map
